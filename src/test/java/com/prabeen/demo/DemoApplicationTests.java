6d16a8fc1bd5ea9f8a580c6d0bd455c64a978c99
prabeen <240853016+prabeenai@users.noreply.github.com>
2025-10-29 11:47:18 +0530

    First application

---
+++ src/test/java/com/prabeen/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabeen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
